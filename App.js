6b0ed28451751d702729db12e8ff48b1e49b4184
Aftar Ahmad Sami <82672770+Aftar-Ahmad-Sami@users.noreply.github.com>
2023-01-27 14:30:25 +0600

    Commit

---
+++ App.js
@@ -0,0 +1,7 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native';
+import Learnig1 from './Learning/learning1';
+
+export default function App() {
+  return Learnig1();
+}
---
+++ Learning/learning1.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native';
+
+export default function Learnig1() {
+    return (
+        <SafeAreaView style={styles.container}>
+
+            <Text>{'Hi\n'}</Text>
+            <Text> I am a dog {'\n'}</Text>
+
+            <Image
+                blurRadius={2}
+                fadeDuration={1000}
+                source={{ width: 200, height: 300, uri: "https://picsum.photos/id/237/200/300" }}
+            // source={require('./assets/favicon.png')}
+            />
+
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
